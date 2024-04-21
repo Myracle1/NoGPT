08de1bfe2e89cea5112653bd212185b155e9b2b8
ZiMSun <98506003+Myracle1@users.noreply.github.com>
2024-04-21 21:35:41 +0800

    pyweb增加SVM分类

--- pyweb/NoGPT.py
+++ pyweb/NoGPT.py
@@ -1,12 +1,14 @@
+"""V3.0：文字+OCR+语音版本+SVM给出分类结果"""
 import base64
 import streamlit as st
 import time
 import cv2
-import pytesseract
+import pytesseract  # OCR:图片转文字
 import numpy as np
 from model import PPL_LL_based_gpt2_t5  # 确保这个路径正确，并包含了您的模型类
+import speech_recognition as sr  # 语音转文字
+import joblib
 
-# 设置页面的一些样式
 def main_bg(main_bg):
     main_bg_ext = "png"
     st.markdown(
@@ -20,7 +22,32 @@ def main_bg(main_bg):
          """,
         unsafe_allow_html=True
     )
+def predict_svm(data):
+    # 加载Python训练的SVM模型
+    svm_model = joblib.load('D:/PyCode/NoGPT/models/new_svm_model.pkl')
+    # 调整数据的形状以匹配模型的输入
+    data = data.reshape(1, -1)
+    # 使用模型进行预测
+    predictions = svm_model.predict(data)
+    probabilities = svm_model.predict_proba(data)
+    return predictions, probabilities
 
+# 从结果中提取出关键指标的数值
+def extract_crucial_value(results):
+    ordered_dict = results[0][0]
+    D_LL = ordered_dict['D_LL']
+    score = ordered_dict['Score']
+    PPL = ordered_dict['Perplexity']
+    data_array = np.array([D_LL, score, PPL])
+    return data_array
+
+def mul_extract_crucial_value(results):
+    results = results[0]
+    D_LL = results['D_LL']
+    score = results['Score']
+    PPL = results['Perplexity']
+    data_array = np.array([D_LL, score, PPL])
+    return data_array
 
 def sidebar_bg(side_bg):
     side_bg_ext = 'png'
@@ -54,13 +81,15 @@ def show_progress(text='正在检测中'):
 # 辅助函数，用于处理文本消息
 def process_text_message(user_msg, model_type):
     model = PPL_LL_based_gpt2_t5()
-    return model(user_msg, model_type, model_type)
+    return model(user_msg, model_type)
 
 # 辅助函数，用于处理文件
 def process_file(file, model_type):
     if file.name.endswith('.txt'):
         texts = read_texts_from_file(file)
         return process_texts(texts, model_type)
+    if file.name.endswith('.wav'):
+        return process_audio(file, model_type)
     else:
         return process_image(file, model_type)
 
@@ -84,6 +113,23 @@ def process_image(file, model_type):
     text = pytesseract.image_to_string(img)
     return process_text_message(text, model_type)
 
+def process_audio(file, model_type):
+    # 创建一个Recognizer对象
+    r = sr.Recognizer()
+    # 使用Google Speech Recognition进行识别
+    try:
+        with sr.AudioFile(file) as source:
+            # listen for the data (load audio to memory)
+            audio_data = r.record(source)
+            text = r.recognize_google(audio_data)
+            st.write("识别的文本是:", text)
+            return process_text_message(text, model_type)
+    except sr.UnknownValueError:
+        st.error("Google Speech Recognition引擎无法理解音频")
+    except sr.RequestError as e:
+        st.error(f"Google Speech Recognition服务出现了错误; {e}")
+
+
 # 主函数
 def main():
     # 调用边框背景
@@ -99,29 +145,51 @@ def main():
     user_msg = st.text_area("👉输入您的消息：")
 
     # 文件上传
-    uploaded_file = st.file_uploader("📂上传文件", type=["txt", "jpg", "png", "jpeg"])
+    uploaded_file = st.file_uploader("📂上传文件", type=["txt", "jpg", "png", "jpeg", "wav"])
 
     # 选择模型类型
-    model_type = st.selectbox('🔑选择模型类型', ['t5-small', 't5-large'], index=0)
+    model_type = st.selectbox('🔑选择模型类型', ['t5-small', 't5-large', 'none'], index=0)
 
     # 开始检测按钮
     start_detect = st.button('⏳开始检测')
-
     # 根据用户输入和上传的文件进行处理
     if start_detect:
         if user_msg:
             show_progress()  # 显示进度条
             results = process_text_message(user_msg, model_type)
             st.write("文本检测结果为：", results)
+            data_array = mul_extract_crucial_value(results)
+            predictions, probabilities = predict_svm(data_array)
+            if predictions == 1:
+                st.write("该文本是由AI生成的")
+            else:
+                st.write("该文本是由人类生成的")
+            st.write("0代表人类生成概率，1代表AI生成概率:", probabilities)
         elif uploaded_file:
             show_progress()  # 显示进度条
             results = process_file(uploaded_file, model_type)
             if isinstance(results, list):
                 st.write("文本检测结果为：", results)
+                data_array = extract_crucial_value(results)
+                predictions, probabilities = predict_svm(data_array)
+                if predictions==1:
+                   st.write("该文本是由AI生成的")
+                else:
+                   st.write("该文本是由人类生成的")
+                st.write("0代表人类生成概率，1代表AI生成概率:", probabilities)
             else:
                 st.write("文本检测结果为：", results)
-
+                print(results)
+                data_array = mul_extract_crucial_value(results)
+                predictions, probabilities = predict_svm(data_array)
+                if predictions==1:
+                   st.write("该文本是由AI生成的")
+                else:
+                   st.write("该文本是由人类生成的")
+                st.write("0代表人类生成概率，1代表AI生成概率:", probabilities)
 
 # 调用主函数
 if __name__ == "__main__":
     main()
+    # 使用提供的示例数据调用函数
+
---
+++ pyweb/train_cn.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import joblib  # 用于保存和加载模型
+
+# 读取human数据
+file_path_human = "./human_data_cn.txt"
+data_human = pd.read_csv(file_path_human, sep="\t", header=None, names=['D_LL', 'Score', 'Perplexity',   'col8', 'col9', 'label'])
+
+# 读取AI数据
+file_path_ai ="./AI_data_cn.txt"
+data_ai = pd.read_csv(file_path_ai, sep="\t", header=None, names=['D_LL', 'Score', 'Perplexity',   'col8', 'col9', 'label'])
+
+# 合并两个数据集
+data = pd.concat([data_human, data_ai])
+
+# 划分训练集和测试集
+train_data, test_data = train_test_split(data, test_size=0.3, random_state=30)
+
+# 特征和标签
+features = train_data[['D_LL', 'Score' ]]
+labels = train_data['label']
+
+# 训练SVM模型
+svm_model = SVC(kernel='rbf', probability=True, random_state=123)
+svm_model.fit(features, labels)
+
+# 保存模型
+joblib.dump(svm_model, 'svm_model_cn.pkl')
+
+# 加载模型
+svm_model_loaded = joblib.load('svm_model_cn.pkl')
+
+# 做预测
+test_features = test_data[['D_LL', 'Score' ]]
+test_labels = test_data['label']
+predictions = svm_model_loaded.predict(test_features)
+
+# 获取预测概率
+probabilities = svm_model_loaded.predict_proba(test_features)
+
+# 输出预测结果和概率
+results = pd.DataFrame({
+  'Actual': test_labels,
+  'Predicted': predictions,
+  'Human_Probability': probabilities[:, 0],
+  'AI_Probability': probabilities[:, 1]
+})
+
+print(results)
+
+# 混淆矩阵和准确率
+conf_matrix = confusion_matrix(test_labels, predictions)
+accuracy = accuracy_score(test_labels, predictions)
+print(conf_matrix)
+print("Accuracy:", accuracy)
+
+# 保存模型到文件
+joblib.dump(svm_model, './models/cn_svm_model.pkl')
+
